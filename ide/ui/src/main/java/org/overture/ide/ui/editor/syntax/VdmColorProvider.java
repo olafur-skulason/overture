8f834bf76e162615b8c1f1b02bc7acdcd09e537f
idhugo <hugodsmacedo@gmail.com>
2017-09-01 09:52:06 +0200

    Removes unused static variables
    Inside the overture the following returns no result beyond the variable
    declaration...
    
    grep -r "VDMDOC" --include='*.java'
    
    grep -r "MULTI_LINE_COMMENT" --include='*.java'

--- ide/ui/src/main/java/org/overture/ide/ui/editor/syntax/VdmColorProvider.java
+++ ide/ui/src/main/java/org/overture/ide/ui/editor/syntax/VdmColorProvider.java
@@ -31,16 +31,11 @@ import org.eclipse.swt.widgets.Display;
 
 public class VdmColorProvider {
 	
-	public static final RGB MULTI_LINE_COMMENT = new RGB(128, 0, 0);
 	public static final RGB SINGLE_LINE_COMMENT = new RGB(63, 127, 95);
 	public static final RGB KEYWORD = new RGB(127, 0, 85);
 	public static final RGB TYPE = new RGB(0, 0, 128);
 	public static final RGB STRING = new RGB(42, 0, 255);
 	public static final RGB DEFAULT = new RGB(0, 0, 0);
-	public static final RGB VDMDOC_KEYWORD = new RGB(0, 128, 0);
-	public static final RGB VDMDOC_TAG = new RGB(128, 128, 128);
-	public static final RGB VDMDOC_LINK = new RGB(128, 128, 128);
-	public static final RGB VDMDOC_DEFAULT = new RGB(0, 128, 128);
 	public static final RGB LATEX = new RGB(153,180,209);
 	
 	protected static Map<RGB, Color> fColorTable = new HashMap<RGB, Color>(10);
